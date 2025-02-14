136f503c0f52fae9790ca895a2b94f96586975ea
FabricioS0 <fsoliveira899@gmail.com>
2025-02-14 17:50:07 -0300

    Atualização no link css de paginas

--- html/Projeto.php
+++ html/Projeto.php
@@ -12,7 +12,7 @@ echo '<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Style/Style_prog_proprietario.css">
+    <link rel="stylesheet" href="/ProjetoPalco/Style/Style_prog_proprietario.css">
     <link rel="stylesheet" href="/ProjetoPalco/Style/cabecalho&header.css">
     <title>Projeto Proprietario</title>
 </head>
